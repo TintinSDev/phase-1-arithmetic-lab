7ba72d4dafe982180d61216e80a3a17dcf0f59fa
Martin <mmaina290@gmail.com>
2023-09-26 20:42:28 +0300

    Succesful

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 10);
+random 
+
+let num3 = 56;
+let num4 = 14;
+const mod = num3 / num4;
+console.log(mod);
+ 
+let max = Math.max(0, -4, 6, 20, 15);
+console.log(max);
